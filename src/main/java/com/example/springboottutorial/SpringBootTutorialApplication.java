861985f6b7a209904926c35cee9037200f0d8262
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2022-12-05 20:32:19 +0700

    create simple api

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -2,12 +2,22 @@ package com.example.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringBootTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
