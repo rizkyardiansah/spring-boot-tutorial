e0dd36a926cc54d39fbe0968a942e0e8386d179e
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2022-12-06 15:59:11 +0700

    add student repository

--- src/main/java/com/example/springboottutorial/student/StudentService.java
+++ src/main/java/com/example/springboottutorial/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.springboottutorial.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,21 +10,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Madara",
-                        "madara.uchiha@gmail.com",
-                        LocalDate.of(1998, Month.FEBRUARY, 28),
-                        25),
-                new Student(
-                        2L,
-                        "Naruto Uzumaki",
-                        "naruto.uzumaki@gmail.com",
-                        LocalDate.of(1999, Month.JULY, 17),
-                        23
-                )
-        );
+        return studentRepository.findAll();
     }
 }
