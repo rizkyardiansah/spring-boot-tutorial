ae0c9a83de9d30afa80b495d64bdd6cd80ccd09a
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2022-12-06 16:37:55 +0700

    change age with transient

--- src/main/java/com/example/springboottutorial/student/StudentConfiguration.java
+++ src/main/java/com/example/springboottutorial/student/StudentConfiguration.java
@@ -14,8 +14,8 @@ public class StudentConfiguration {
     @Bean
     public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student madara = new Student("Madara", "madara@gmail.com", LocalDate.of(2000, Month.AUGUST, 24), 24);
-            Student naruto = new Student("Naruto", "naruto@gmail.com", LocalDate.of(2000, Month.JULY, 10), 21);
+            Student madara = new Student("Madara", "madara@gmail.com", LocalDate.of(1998, Month.AUGUST, 24));
+            Student naruto = new Student("Naruto", "naruto@gmail.com", LocalDate.of(2002, Month.JULY, 10));
 
             studentRepository.saveAll(List.of(madara, naruto));
         };
